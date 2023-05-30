f5036de6721702c0344a7e30c98dbe6800d8fb59
Pablo <106776147+JPablohenao@users.noreply.github.com>
2023-05-30 15:10:31 -0500

    Doble Cara
    Crear una página en la que inicialmente se muestre una imagen, Al darle clic se debe mostrar la otra imagen en vez de la anterior

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [imageNumber, setImageNumber] = useState(0)
+
+  const changeImage = () => {
+    (imageNumber === 0) ? setImageNumber(1) : setImageNumber(0)
+  }
+  let imagenes = [
+    require("./img/one-black.png"),
+    require("./img/one-blue.png")
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={imagenes[imageNumber]} onClick={changeImage} />
+        <p> Double Sided    </p>
+
       </header>
-    </div>
+    </div >
   );
 }
 
